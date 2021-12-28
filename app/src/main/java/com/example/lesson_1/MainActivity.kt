e8ff7dc993333a47ec4ef0bb236edb1e4a63a607
lekoal <coreglus@gmail.com>
2021-12-28 17:48:55 +0300

    task 4

--- app/src/main/java/com/example/lesson_1/MainActivity.kt
+++ app/src/main/java/com/example/lesson_1/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.lesson_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.lesson_1.R
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button: Button = findViewById(R.id.button)
+
+        button.setOnClickListener {
+            Toast.makeText(this, "''PUSH ME'' button is pressed", Toast.LENGTH_SHORT).show()
+        }
     }
 }
