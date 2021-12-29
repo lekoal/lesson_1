b54c937e06d3154025749f217f3e1f9289118f07
lekoal <coreglus@gmail.com>
2021-12-29 13:38:40 +0300

    task 5c

--- app/src/main/java/com/example/lesson_1/MainActivity.kt
+++ app/src/main/java/com/example/lesson_1/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
 
         val button1: Button = findViewById(R.id.button1)
         val button2: Button = findViewById(R.id.button2)
+        val button3: Button = findViewById(R.id.button3)
         val textView1: TextView = findViewById(R.id.textView1)
         val textView2: TextView = findViewById(R.id.textView2)
 
@@ -21,18 +22,49 @@ class MainActivity : AppCompatActivity() {
 
         val myObject = ExampleObjectSingleton
 
+        val myDays: List<ExampleEnumClass> = ExampleEnumClass.values().toList()
+
         button1.setOnClickListener {
-            Toast.makeText(this, "''PUSH ME'' button 1 is pressed", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "''TASK 5A'' button 1 is pressed", Toast.LENGTH_SHORT).show()
             textView1.text = "Data class date: " + myExample.myDate
             textView2.text = "Data class count: " + myExample.myCount.toString()
         }
 
         button2.setOnClickListener {
-            Toast.makeText(this, "''PUSH ME'' button 2 is pressed", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "''TASK 5B'' button 2 is pressed", Toast.LENGTH_SHORT).show()
             myObject.classCopy(myExample)
             textView1.text = "Object class date: " + myObject.getMyDate()
             textView2.text = "Object class count: " + myObject.getMyCount()
         }
 
+        button3.setOnClickListener {
+            Toast.makeText(this, "''TASK 5C'' button 3 is pressed", Toast.LENGTH_SHORT).show()
+
+            for (i in myDays) {
+                println(i)
+            }
+            println("---------------------------------")
+
+            for (i in 0 until myDays.size step 2) {
+                println(myDays[i])
+            }
+            println("---------------------------------")
+
+            for (i in myDays.size - 1 downTo 0) {
+                println(myDays[i])
+            }
+            println("---------------------------------")
+
+            for (i in myDays.indices) {
+                println(i)
+            }
+            println("---------------------------------")
+
+            var i = 0
+            while (i < myDays.size) {
+                println(myDays[i])
+                i++
+            }
+        }
     }
 }
